f96315ad1a1a5dc4a00532de3d949beeebceeb8e
unknown <rostykvoznyak@gmail.com>
2020-11-03 13:48:19 +0200

    Lesson6Task1

---
+++ Lesson6Task1/Lesson6Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson6Task1
+{
+    class Program
+    {
+        static int CalculateIncrement(ref int a)
+        {
+            int result = ++a;
+            return result;
+        }
+        static int CalculateDecriment(ref int a)
+        {
+            int result = --a;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int i = 9;
+            int result1 = CalculateIncrement(ref i);
+            Console.WriteLine(result1);
+
+            int result2 = CalculateDecriment(ref i);
+            Console.WriteLine(result2);
+        }
+    }
+}
---
+++ Lesson6Task1/Lesson6Task2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Data;
+
+namespace Lesson6Task2
+{
+    class Program
+    {
+        static int plusNumber(int a, int b)
+        {
+            int result = a + b;
+            return result;
+        }
+        static int minusNumber(int a, int b)
+        {
+            int result = a - b;
+            return result;
+        }
+        static int mnojNumber(int a, int b)
+        {
+            int result = a * b;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number 1:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Number 2:");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your action:");
+            string k = Console.ReadLine();
+
+            switch(k)
+            {
+                case ("+"):
+                    {
+                        int f = plusNumber(number1, number2);
+                        Console.WriteLine(f);
+                        break;
+                    }
+                case ("-"):
+                    {
+                        int f = minusNumber(number1, number2);
+                        Console.WriteLine(f);
+                        break;
+                    }
+                case ("*"):
+                    {
+                        int f = mnojNumber(number1, number2);
+                        Console.WriteLine(f);
+                        break;
+                    }
+            }
+        }
+    }
+}
